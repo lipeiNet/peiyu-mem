8f8cf90ac3cb73d93ae501c44e4ef938c0c756f0
lipei <lipei@Lenovo-PC>
2016-11-30 13:16:39 +0800

    活动dao的接口

---
+++ mem-dao/src/main/java/com/peiyu/mem/dao/CpActivityDao.java
@@ -0,0 +1,25 @@
+package com.peiyu.mem.dao;
+
+import com.peiyu.mem.domian.entity.CpActivity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/11/29.
+ */
+@Repository
+public interface CpActivityDao extends BaseDao<CpActivity>{
+    /**
+     * 根据商家id获取优惠券活动
+     * @param venorId
+     * @return
+     */
+    List<CpActivity> getCpActivitysByVendorId(Long venorId);
+    /**
+     * 通过优惠券活动属性来查询优惠券活动
+     * @param cpActivity
+     * @return
+     */
+    List<CpActivity> getCpActivityBySearch(CpActivity cpActivity);
+}
