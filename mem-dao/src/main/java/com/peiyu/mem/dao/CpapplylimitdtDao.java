d881212cce14048bfa71949c3bcea9e5162f9fb0
lipei <lipei@Lenovo-PC>
2016-12-02 21:27:37 +0800

    添加批量添加和更新接口

--- mem-dao/src/main/java/com/peiyu/mem/dao/CpapplylimitdtDao.java
+++ mem-dao/src/main/java/com/peiyu/mem/dao/CpapplylimitdtDao.java
@@ -1,6 +1,7 @@
 package com.peiyu.mem.dao;
 
 import com.peiyu.mem.domian.entity.CpApplyLimitdt;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,17 @@ import java.util.List;
 @Repository
 public interface CpapplylimitdtDao extends BaseDao<CpApplyLimitdt> {
 
-
+    /**
+     * 批量插入应用范围限制
+     * @return
+     */
+    int insertBatchApplylimits(@Param("applyLimitdts") List<CpApplyLimitdt> applyLimitdts);
+    /**
+     * 批量更新应用范围限制
+     * @param applyLimitdts
+     * @return
+     */
+    int updateBatchApplylimits(@Param("applyLimitdts") List<CpApplyLimitdt> applyLimitdts);
     /**
      * 根据条件获取应用范围
      * @param Search
