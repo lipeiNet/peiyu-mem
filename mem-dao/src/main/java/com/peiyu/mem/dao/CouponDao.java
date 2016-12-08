9fcba62f6143a368144af1e535ce60aff7daeaac
lipei <lipei@Lenovo-PC>
2016-12-08 18:21:00 +0800

    加入批量添加优惠券

--- mem-dao/src/main/java/com/peiyu/mem/dao/CouponDao.java
+++ mem-dao/src/main/java/com/peiyu/mem/dao/CouponDao.java
@@ -1,6 +1,7 @@
 package com.peiyu.mem.dao;
 
 import com.peiyu.mem.domian.entity.Coupon;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +11,17 @@ import java.util.List;
  */
 @Repository
 public interface CouponDao extends BaseDao<Coupon> {
+
     /**
      * 根据条件获取优惠券
      * @param serach
      * @return
      */
     List<Coupon> getCouponsBySearch(Coupon serach);
+
+    /**
+     * 批量插入优惠券
+     * @param coupons
+     */
+    void insertBatchCoupons(@Param("coupons") List<Coupon> coupons);
 }
