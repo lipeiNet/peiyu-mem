104f8a78305046ff42b89b26613eb424705930db
lipei <lipei@Lenovo-PC>
2016-12-08 15:08:37 +0800

    添加对制券编码获取制券信息

--- mem-dao/src/main/java/com/peiyu/mem/dao/CpMakingTaskDao.java
+++ mem-dao/src/main/java/com/peiyu/mem/dao/CpMakingTaskDao.java
@@ -1,6 +1,7 @@
 package com.peiyu.mem.dao;
 
 import com.peiyu.mem.domian.entity.CpMakingTask;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,13 @@ import java.util.List;
  */
 @Repository
 public interface CpMakingTaskDao extends BaseDao<CpMakingTask> {
+    /**
+     * 根据制券编码获取制券信息
+     * @param vendorId
+     * @param taskCode
+     * @return
+     */
+    CpMakingTask getCpMakingTaskByTaskCode(@Param("vendorId") Long vendorId,@Param("taskCode") String taskCode);
     /**
      * 根据条件获取制券的任务
      * @param search
