fac554b011e97826d7e540c078d7aa952c21db82
lipei <lipei@Lenovo-PC>
2016-12-09 00:04:09 +0800

    发送消息

---
+++ mem-rabbitmq/src/main/java/com/peiyu/mem/rabbitmq/MqSenderHandler.java
@@ -0,0 +1,29 @@
+package com.peiyu.mem.rabbitmq;
+
+import org.apache.log4j.Logger;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+@Component
+public class MqSenderHandler {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    private Logger log = Logger.getLogger(MqSenderHandler.class);
+
+    /**
+     * 发送信息
+     *
+     * @param messageInfo
+     */
+    public void sendMessage(String routingKey,Object messageInfo) {
+        try {
+            rabbitTemplate.convertAndSend(routingKey,messageInfo);
+        } catch (Exception e) {
+            log.error("发送消息失败");
+        }
+    }
+}
