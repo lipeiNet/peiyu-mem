78ab143839e72ec2024f1bbeaa57c40c133c263e
lipei <lipei@Lenovo-PC>
2016-12-09 17:57:13 +0800

    修改消息队列制券

--- mem-rabbitmq/src/main/java/com/peiyu/mem/rabbitmq/consumers/MakeCouponsHandler.java
+++ mem-rabbitmq/src/main/java/com/peiyu/mem/rabbitmq/consumers/MakeCouponsHandler.java
@@ -1,24 +1,29 @@
-package com.peiyu.mem.rabbitmq;
+package com.peiyu.mem.rabbitmq.consumers;
 
 import com.google.gson.reflect.TypeToken;
 import com.migr.common.util.JsonUtil;
+import com.migr.common.util.StringUtils;
 import com.peiyu.mem.dao.CouponDao;
 import com.peiyu.mem.domian.entity.Coupon;
+import com.peiyu.mem.rabbitmq.Gson2JsonMessageConverter;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/8.
  */
+@Component
 public class MakeCouponsHandler implements MessageListener {
     private Logger log = Logger.getLogger(MakeCouponsHandler.class);
     @Autowired
     private CouponDao couponDao;
+    private Gson2JsonMessageConverter jsonMessageConverter;
 
     @Override
     public void onMessage(Message message) {
@@ -26,13 +31,21 @@ public class MakeCouponsHandler implements MessageListener {
             if (message == null || message.getBody() == null) {
                 return;
             }
-            String body = new String(message.getBody(), "UTF-8");
-            List<Coupon> coupons = JsonUtil.g.fromJson(body, new TypeToken<List<Coupon>>() {
-            }.getType());
-            couponDao.insertBatchCoupons(coupons);
+            String data=jsonMessageConverter.fromMessage(message).toString();
+            if (StringUtils.isNotBlank(data)){
+                List<Coupon> coupons= JsonUtil.g.fromJson(data,new TypeToken<List<Coupon>>(){}.getType());
+                couponDao.insertBatchCoupons(coupons);
+            }
         } catch (Exception e) {
             log.error("消息队列处理制券异常：" + e);
-            System.out.println("消息队列处理制券异常：" + e.getMessage());
         }
+    }
+
+    public Gson2JsonMessageConverter getJsonMessageConverter() {
+        return jsonMessageConverter;
+    }
+
+    public void setJsonMessageConverter(Gson2JsonMessageConverter jsonMessageConverter) {
+        this.jsonMessageConverter = jsonMessageConverter;
     }
 }
