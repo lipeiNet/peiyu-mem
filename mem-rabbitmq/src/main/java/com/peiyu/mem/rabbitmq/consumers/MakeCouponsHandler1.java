0014e862f67e1c9f694ad809b5f675bca4cf7ba2
lipei <lipei@Lenovo-PC>
2016-12-12 15:00:45 +0800

    修改消费者

--- mem-rabbitmq/src/main/java/com/peiyu/mem/rabbitmq/consumers/MakeCouponsHandler1.java
+++ mem-rabbitmq/src/main/java/com/peiyu/mem/rabbitmq/consumers/MakeCouponsHandler1.java
@@ -6,37 +6,41 @@ import com.migr.common.util.StringUtils;
 import com.peiyu.mem.dao.CouponDao;
 import com.peiyu.mem.domian.entity.Coupon;
 import com.peiyu.mem.rabbitmq.Gson2JsonMessageConverter;
+import com.rabbitmq.client.Channel;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/8.
  */
 @Component
-public class MakeCouponsHandler implements MessageListener {
-    private Logger log = Logger.getLogger(MakeCouponsHandler.class);
+public class MakeCouponsHandler1 implements ChannelAwareMessageListener {
+    private Logger log = Logger.getLogger(MakeCouponsHandler1.class);
     @Autowired
     private CouponDao couponDao;
     private Gson2JsonMessageConverter jsonMessageConverter;
-
     @Override
-    public void onMessage(Message message) {
+    public void onMessage(Message message, Channel channel) throws Exception {
         try {
+            channel.basicQos(1);
             if (message == null || message.getBody() == null) {
                 return;
             }
-            String data=jsonMessageConverter.fromMessage(message).toString();
-            if (StringUtils.isNotBlank(data)){
-                List<Coupon> coupons= JsonUtil.g.fromJson(data,new TypeToken<List<Coupon>>(){}.getType());
+            String data = jsonMessageConverter.fromMessage(message).toString();
+            if (StringUtils.isNotBlank(data)) {
+                List<Coupon> coupons = JsonUtil.g.fromJson(data, new TypeToken<List<Coupon>>() {
+                }.getType());
                 couponDao.insertBatchCoupons(coupons);
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+                System.out.println("1类："+message.getMessageProperties().getDeliveryTag());
             }
         } catch (Exception e) {
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), true, true);
             log.error("消息队列处理制券异常：" + e);
         }
     }
