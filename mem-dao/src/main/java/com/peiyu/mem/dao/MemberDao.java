f699d8e0b6ffa1c57382b034e4d3a47f3bca238a
lipei <lipei@Lenovo-PC>
2016-12-13 18:14:18 +0800

    添加会员dao

---
+++ mem-dao/src/main/java/com/peiyu/mem/dao/MemberDao.java
@@ -0,0 +1,10 @@
+package com.peiyu.mem.dao;
+
+import com.peiyu.mem.domian.entity.Member;
+
+/**
+ * Created by Administrator on 2016/12/13.
+ */
+public interface MemberDao extends BaseDao<Member> {
+
+}
