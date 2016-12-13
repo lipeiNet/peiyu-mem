91f8b4e10fcc9157c7f50897a6a1bba53f25880e
lipei <lipei@Lenovo-PC>
2016-12-13 18:27:02 +0800

    加入根据商家获取会员信息方法

--- mem-dao/src/main/java/com/peiyu/mem/dao/MemberDao.java
+++ mem-dao/src/main/java/com/peiyu/mem/dao/MemberDao.java
@@ -1,10 +1,17 @@
 package com.peiyu.mem.dao;
 
 import com.peiyu.mem.domian.entity.Member;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by Administrator on 2016/12/13.
  */
 public interface MemberDao extends BaseDao<Member> {
-
+    /**
+     * 根据商家获取会员信息
+     * @param vendorId
+     * @param memNo 会员编号
+     * @return
+     */
+    Member getMemberByMemNo(@Param("vendorId") Long vendorId, @Param("memNo") String memNo);
 }
