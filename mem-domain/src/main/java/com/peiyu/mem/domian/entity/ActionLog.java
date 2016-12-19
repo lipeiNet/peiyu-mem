047419ec2065809e25f20278f5e1f780b2fe4009
lipei <lipei@Lenovo-PC>
2016-12-19 17:54:56 +0800

    操作日志实体

--- mem-domain/src/main/java/com/peiyu/mem/domian/entity/ActionLog.java
+++ mem-domain/src/main/java/com/peiyu/mem/domian/entity/ActionLog.java
@@ -1,5 +1,7 @@
 package com.peiyu.mem.domian.entity;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2016/12/19.
  */
@@ -7,5 +9,42 @@ public class ActionLog {
     /**
      * 操作记录
      */
-    private int id;
+    private Long id;
+    /**
+     * 商家id
+     */
+    private Long vendorId;
+    /**
+     * 会员编号
+     */
+    private String memNo;
+    /**
+     * 类名
+     */
+    private String className;
+    /**
+     * 方法类别（0：添加，1删除，2修改，3查询）
+     */
+    private int methodType;
+    /**
+     * 方法名
+     */
+    private String methodName;
+    /**
+     * 方法参数
+     */
+    private String methodParam;
+    /**
+     * 参数值
+     */
+    private String paramValue;
+    /**
+     * 方法效率（单位毫秒）
+     */
+    private Long operationTime;
+    /**
+     * 操作时间
+     */
+    private Date createDate;
+
 }
