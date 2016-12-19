a2d2aa53cf4435eac7281936b096a5c1d6ffc09f
lipei <lipei@Lenovo-PC>
2016-12-19 18:25:47 +0800

    异常日志接口

---
+++ mem-dao/src/main/java/com/peiyu/mem/dao/AbnormalLogDao.java
@@ -0,0 +1,9 @@
+package com.peiyu.mem.dao;
+
+import com.peiyu.mem.domian.entity.AbnormalLog;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+public interface AbnormalLogDao extends BaseDao<AbnormalLog> {
+}
