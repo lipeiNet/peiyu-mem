ee20b529e9dfe069e11898cf0672bc6ec64f1972
lipei <lipei@Lenovo-PC>
2016-12-19 18:26:04 +0800

    操作日志接口

---
+++ mem-dao/src/main/java/com/peiyu/mem/dao/ActionLogDao.java
@@ -0,0 +1,9 @@
+package com.peiyu.mem.dao;
+
+import com.peiyu.mem.domian.entity.ActionLog;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+public interface ActionLogDao extends BaseDao<ActionLog> {
+}
