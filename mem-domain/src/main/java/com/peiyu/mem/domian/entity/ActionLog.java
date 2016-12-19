fa961b2708fb1da30388b0c729fdd85ed470649e
lipei <lipei@Lenovo-PC>
2016-12-19 17:55:26 +0800

    操作日志实体

--- mem-domain/src/main/java/com/peiyu/mem/domian/entity/ActionLog.java
+++ mem-domain/src/main/java/com/peiyu/mem/domian/entity/ActionLog.java
@@ -47,4 +47,83 @@ public class ActionLog {
      */
     private Date createDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(Long vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getMemNo() {
+        return memNo;
+    }
+
+    public void setMemNo(String memNo) {
+        this.memNo = memNo;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getMethodType() {
+        return methodType;
+    }
+
+    public void setMethodType(int methodType) {
+        this.methodType = methodType;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getMethodParam() {
+        return methodParam;
+    }
+
+    public void setMethodParam(String methodParam) {
+        this.methodParam = methodParam;
+    }
+
+    public String getParamValue() {
+        return paramValue;
+    }
+
+    public void setParamValue(String paramValue) {
+        this.paramValue = paramValue;
+    }
+
+    public Long getOperationTime() {
+        return operationTime;
+    }
+
+    public void setOperationTime(Long operationTime) {
+        this.operationTime = operationTime;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 }
