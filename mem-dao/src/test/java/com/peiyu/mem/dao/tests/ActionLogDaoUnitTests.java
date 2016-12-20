77d3becf6de65e48db4d64bdbc2824ce58460c19
lipei <lipei@Lenovo-PC>
2016-12-20 14:06:02 +0800

    添加操作日志单元测试

---
+++ mem-dao/src/test/java/com/peiyu/mem/dao/tests/ActionLogDaoUnitTests.java
@@ -0,0 +1,72 @@
+package com.peiyu.mem.dao.tests;
+
+import com.peiyu.mem.dao.ActionLogDao;
+import com.peiyu.mem.dao.BaseDao;
+import com.peiyu.mem.domian.entity.ActionLog;
+import com.peiyu.mem.utils.DateUtil;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
+import org.unitils.reflectionassert.ReflectionAssert;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+public class ActionLogDaoUnitTests extends BaseDaoUnitTests{
+    @Autowired
+    private ActionLogDao actionLogDao;
+    @Test
+    public void testInsert(){
+        ActionLog actionLog=new ActionLog();
+        actionLog.setClassName("actionlog");
+        actionLog.setCreateDate(new Date());
+        actionLog.setMemNo("123");
+        actionLog.setMethodName("insert");
+        actionLog.setMethodParam("123");
+        actionLog.setOperationTime(1000l);
+        actionLog.setMethodType(1);
+        actionLog.setVendorId(1433l);
+        actionLogDao.insert(actionLog);
+    }
+    @Test
+    public void testUpdate(){
+        ActionLog actionLog=new ActionLog();
+        actionLog.setId(1l);
+        actionLog.setClassName("actionlog");
+        actionLog.setCreateDate(new Date());
+        actionLog.setMemNo("123");
+        actionLog.setMethodName("insert");
+        actionLog.setMethodParam("123");
+        actionLog.setOperationTime(1000l);
+        actionLog.setMethodType(1);
+        actionLog.setVendorId(1433l);
+    }
+    @Test
+    public void testDelete(){
+        long id=1;
+        assertEquals(1,actionLogDao.delete(id));
+    }
+    @Test
+    public void testGet(){
+        ActionLog except=getActionLog();
+        ActionLog result=actionLogDao.get(1l);
+        ReflectionAssert.assertReflectionEquals(except,result);
+
+    }
+    protected ActionLog getActionLog(){
+        ActionLog actionLog=new ActionLog();
+        actionLog.setId(1l);
+        actionLog.setClassName("actionlog");
+        actionLog.setCreateDate(DateUtil.getFormatDate("2016-12-20","yyyy-MM-dd"));
+        actionLog.setMemNo("156234");
+        actionLog.setParamValue("123");
+        actionLog.setMethodName("insert");
+        actionLog.setMethodParam("2312");
+        actionLog.setOperationTime(1000l);
+        actionLog.setMethodType(1);
+        actionLog.setVendorId(1433l);
+        return actionLog;
+    }
+}
