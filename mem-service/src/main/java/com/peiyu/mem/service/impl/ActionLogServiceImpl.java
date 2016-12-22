e7991233db3946bb9cd721d50d91189a7c8d5b24
lipei <lipei@Lenovo-PC>
2016-12-22 20:30:08 +0800

    重构操作日志类

--- mem-service/src/main/java/com/peiyu/mem/service/impl/ActionLogServiceImpl.java
+++ mem-service/src/main/java/com/peiyu/mem/service/impl/ActionLogServiceImpl.java
@@ -6,6 +6,7 @@ import com.peiyu.mem.domian.entity.ActionLog;
 import com.peiyu.mem.domian.entity.Member;
 import com.peiyu.mem.rabbitmq.produces.MqSenderHandler;
 import com.peiyu.mem.service.ActionLogService;
+import com.peiyu.mem.utils.ParamUtils;
 import javassist.*;
 import javassist.bytecode.CodeAttribute;
 import javassist.bytecode.LocalVariableAttribute;
@@ -78,7 +79,7 @@ public class ActionLogServiceImpl implements ActionLogService {
             Class<?> clazz = Class.forName(classType);
             String clazzName = clazz.getName();
             String[] methodParams;
-            methodParams = this.getPavamsName(clazz, clazzName, methodName);
+            methodParams = ParamUtils.getPavamsName(clazz, clazzName, methodName);
             if (methodParams != null && methodParams.length > 0) {
                 for (String str : methodParams) {
                     methodParam.append(str+",");
@@ -89,15 +90,10 @@ public class ActionLogServiceImpl implements ActionLogService {
         if (args != null && args.length > 0) {
             for (Object obj : args) {
                 String typeName = obj.getClass().getName();
-                boolean clazzFlag = true;
-                for (String t : types) {
-                    if (t.equals(typeName)) {
-                        clazzFlag = false;
-                        pavamValues.append(obj + ",");
-                    }
-                }
-                if (clazzFlag) {
-                    pavamValues.append(this.getFieldsValue(obj) + ",");
+                if (ParamUtils.isBasicType(typeName)) {
+                    pavamValues.append(obj + ",");
+                } else {
+                    pavamValues.append(ParamUtils.getFieldsValue(obj) + ",");
                 }
             }
         }
@@ -122,70 +118,4 @@ public class ActionLogServiceImpl implements ActionLogService {
         senderHandler.sendMessage("spring.actionLog.queueKey", data);
         return true;
     }
-    /**
-     * 获取参数名称
-     * @param cls
-     * @param clazzName
-     * @param methodName
-     * @return
-     * @throws NotFoundException
-     */
-    protected String[] getPavamsName(Class cls, String clazzName, String methodName) throws NotFoundException {
-        ClassPool pool = ClassPool.getDefault();
-        ClassClassPath classPath = new ClassClassPath(cls);
-        pool.insertClassPath(classPath);
-
-        CtClass cc = pool.get(clazzName);
-        CtMethod cm = cc.getDeclaredMethod(methodName);
-        MethodInfo methodInfo = cm.getMethodInfo();
-        CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
-        LocalVariableAttribute attr = (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag);
-        if (attr == null) {
-            return null;
-        }
-        String[] paramNames = new String[cm.getParameterTypes().length];
-        int pos = Modifier.isStatic(cm.getModifiers()) ? 0 : 1;
-        for (int i = 0; i < paramNames.length; i++){
-            paramNames[i] = attr.variableName(i + pos+1); //paramNames即参数名
-        }
-
-        return paramNames;
-    }
-
-    /**
-     * 获取对象的属性值
-     * @param obj
-     * @return
-     */
-    public String getFieldsValue(Object obj) {
-        Field[] fields = obj.getClass().getDeclaredFields();
-        String typeName = obj.getClass().getName();
-        for (String t : types) {
-            if (t.equals(typeName))
-                return "";
-        }
-        StringBuilder sb = new StringBuilder();
-        if (fields != null && fields.length > 0) {
-            sb.append("【");
-            for (Field f : fields) {
-                f.setAccessible(true);
-                for (String str : types) {
-                    if (f.getType().getName().equals(str)) {
-                        try {
-                            sb.append(f.get(obj) + ",");
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-            sb.append("】");
-        }
-        return sb.toString();
-    }
-    private static String[] types = { "java.lang.Integer", "java.lang.Double",
-            "java.lang.Float", "java.lang.Long", "java.lang.Short",
-            "java.lang.Byte", "java.lang.Boolean", "java.lang.Char",
-            "java.lang.String", "int", "double", "long", "short", "byte",
-            "boolean", "char", "float" };
 }
