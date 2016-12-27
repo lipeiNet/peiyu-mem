40b00015fbc42d4ccde9ff98e37bb6b9022dbc0e
lipei <lipei@Lenovo-PC>
2016-12-27 17:36:55 +0800

    券组服务测试

--- mem-service/src/test/java/com/peiyu/mem/tests/CpActsubGroupServiceUnitTests.java
+++ mem-service/src/test/java/com/peiyu/mem/tests/CpActsubGroupServiceUnitTests.java
@@ -31,7 +31,7 @@ public class CpActsubGroupServiceUnitTests {
         actsubGroup.setVendorId(1483l);
         actsubGroup.setActName("消费送券001");
         actsubGroup.setActNo("VD2016121600001");
-        actsubGroup.setSubgroupCode(bizService.getOddNumbers(1483l,"ActsubGroup_Codes",0));
+        actsubGroup.setSubgroupCode(bizService.getOddNumbers(1483l,"ActsubGroup_Code",0));
         actsubGroup.setCpValue(10d);
         actsubGroup.setCpLimitValue(200d);
         actsubGroup.setCpType("0");
@@ -56,37 +56,40 @@ public class CpActsubGroupServiceUnitTests {
     }
     @Test
     public void testInsertActsubGroup1(){
-        for (int i=0;i<5;i++){
-            CpActsubGroup actsubGroup=new CpActsubGroup();
-            actsubGroup.setVendorId(1468l);
-            actsubGroup.setActName("消费送券002");
-            actsubGroup.setActNo("VD2016121900010");
-            actsubGroup.setSubgroupCode(bizService.getOddNumbers(1468l,"ActsubGroup_Codes",0));
-            actsubGroup.setCpValue(10d);
-            actsubGroup.setCpLimitValue(200d);
-            actsubGroup.setCpType("0");
-            actsubGroup.setInheritActLimit(1);
-            actsubGroup.setStartDate(DateUtil.getFormatDate("2016-12-15","yyyy-MM-dd"));
-            actsubGroup.setEndDate(DateUtil.getFormatDate("2016-12-25","yyyy-MM-dd"));
-            actsubGroup.setGetLimitType(SysConstants.GETCOUPONLIMITSTATE.UNLIMITED);
-            actsubGroup.setUseLimitType(SysConstants.USECOUPONLIMITSTATE.UNLIMITED);
-            actsubGroup.setAllowSimt(0);
-            actsubGroup.setWeekFlag("1111111");
-            actsubGroup.setApplyScopeType(SysConstants.COUPONAPPLIEDRANGE.CATEGORY);
-            actsubGroup.setOrganOrStoreCode("10,101,910003");
-            actsubGroup.setOrganOrStoreName("91动力集团,总部机构,91分公司");
-            actsubGroup.setUseScopeType(SysConstants.COUPONUSERANGE.ORAGN);
-            actsubGroup.setDetailName("百岁山,非统配品牌,康师傅,中石化,子公司统配品牌,总公司统配品牌");
-            actsubGroup.setDetailCode("pp30,10,pp29,pp31,99,999");
-            actsubGroup.setMemo("备注");
-            actsubGroup.setCreator("lp");
-            actsubGroup.setModifyDate(new Date());
-            actsubGroup.setCreateDate(new Date());
-            actsubGroup.setDf(0);
-            actsubGroup.setSendType(SysConstants.CouponSendType.AUTO_PROVIDE);
-            actsubGroup.setOverMoney(100d);
-            actsubGroup.setUseOverMoney(200d);
-            actsubGroupService.insertActsubGroup(actsubGroup);
+        for (int j=1;j<25;j++){
+            for (int i=1;i<3;i++){
+                CpActsubGroup actsubGroup=new CpActsubGroup();
+                actsubGroup.setVendorId(1483l);
+                actsubGroup.setActName("消费送券0"+j);
+                actsubGroup.setActNo("VD201612270000"+(j+1));
+                actsubGroup.setSubgroupCode(bizService.getOddNumbers(1483l,"ActsubGroup_Code",0));
+                actsubGroup.setCpValue(10d*i);
+                actsubGroup.setCpLimitValue(200d);
+                actsubGroup.setCpType("0");
+                actsubGroup.setInheritActLimit(1);
+                actsubGroup.setStartDate(DateUtil.getFormatDate("2016-12-15","yyyy-MM-dd"));
+                actsubGroup.setEndDate(DateUtil.getFormatDate("2017-12-25","yyyy-MM-dd"));
+                actsubGroup.setGetLimitType(SysConstants.GETCOUPONLIMITSTATE.UNLIMITED);
+                actsubGroup.setUseLimitType(SysConstants.USECOUPONLIMITSTATE.UNLIMITED);
+                actsubGroup.setAllowSimt(0);
+                actsubGroup.setWeekFlag("1111111");
+                actsubGroup.setApplyScopeType(SysConstants.COUPONAPPLIEDRANGE.CATEGORY);
+                actsubGroup.setOrganOrStoreCode("10,101,910003");
+                actsubGroup.setOrganOrStoreName("91动力集团,总部机构,91分公司");
+                actsubGroup.setUseScopeType(SysConstants.COUPONUSERANGE.ORAGN);
+                actsubGroup.setDetailName("百岁山,非统配品牌,康师傅,中石化,子公司统配品牌,总公司统配品牌");
+                actsubGroup.setDetailCode("pp30,10,pp29,pp31,99,999");
+                actsubGroup.setMemo("备注");
+                actsubGroup.setCreator("lp");
+                actsubGroup.setModifyDate(new Date());
+                actsubGroup.setCreateDate(new Date());
+                actsubGroup.setDf(0);
+                actsubGroup.setSendType(SysConstants.CouponSendType.AUTO_PROVIDE);
+                actsubGroup.setOverMoney(100d);
+                actsubGroup.setUseOverMoney(200d);
+                actsubGroupService.insertActsubGroup(actsubGroup);
+            }
         }
+
     }
 }
