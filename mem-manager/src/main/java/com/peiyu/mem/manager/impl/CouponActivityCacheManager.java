969a61191c01070a4641c2a5a6ddf074ea2a7262
lipei <lipei@Lenovo-PC>
2017-01-09 17:07:40 +0800

    加入活动缓存

--- mem-manager/src/main/java/com/peiyu/mem/manager/impl/CouponActivityCacheManager.java
+++ mem-manager/src/main/java/com/peiyu/mem/manager/impl/CouponActivityCacheManager.java
@@ -1,15 +1,18 @@
 package com.peiyu.mem.manager.impl;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import com.migr.common.util.JsonUtil;
 import com.migr.common.util.StringUtils;
 import com.peiyu.mem.dao.CpActivityDao;
+import com.peiyu.mem.dao.CpActsubGroupDao;
+import com.peiyu.mem.dao.CpapplylimitdtDao;
+import com.peiyu.mem.dao.CpuselimitdtDao;
 import com.peiyu.mem.domian.entity.CpActivity;
-import com.peiyu.mem.rabbitmq.Gson2JsonMessageConverter;
+import com.peiyu.mem.domian.entity.CpActsubGroup;
+import com.peiyu.mem.domian.entity.CpApplyLimitdt;
+import com.peiyu.mem.domian.entity.CpUseLimitdt;
 import com.peiyu.mem.redis.JedisTemplate;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +30,12 @@ public class CouponActivityCacheManager {
     private Logger log = Logger.getLogger(CouponActivityCacheManager.class);
     @Autowired
     private CpActivityDao activityDao;
+    @Autowired
+    private CpActsubGroupDao actsubGroupDao;
+    @Autowired
+    private CpapplylimitdtDao applyLimitdtDao;
+    @Autowired
+    private CpuselimitdtDao cpuselimitdtDao;
     @Autowired
     private JedisTemplate jedisTemplate;
     @Autowired
@@ -77,31 +86,80 @@ public class CouponActivityCacheManager {
                 for (final JsonElement elem : array) {
                     list.add(JsonUtil.g.fromJson(elem, type));
                 }
-               /* list = JsonUtil.g.fromJson(jsonList, new TypeToken<List<T>>() {
-                }.getType());*/
                 return list;
             }
             return list;
         }
     }
 
-    private ListGetterHandler<CpActivity> activityListGetterHandler=new ListGetterHandler<CpActivity>() {
+    private ListGetterHandler<CpActivity> activityListGetterHandler = new ListGetterHandler<CpActivity>() {
         @Override
         List<CpActivity> getListFromDB(CpActivity search) {
             return activityDao.getCpActivityBySearch(search);
         }
     };
 
+    private ListGetterHandler<CpActsubGroup> actsubGroupListGetterHandler = new ListGetterHandler<CpActsubGroup>() {
+        @Override
+        List<CpActsubGroup> getListFromDB(CpActsubGroup search) {
+            return actsubGroupDao.getCpActsubGroupList(search);
+        }
+    };
+
+    private ListGetterHandler<CpApplyLimitdt> applyLimitdtListGetterHandler = new ListGetterHandler<CpApplyLimitdt>() {
+        @Override
+        List<CpApplyLimitdt> getListFromDB(CpApplyLimitdt search) {
+            return applyLimitdtDao.getCpApplyLimitdtsBySearch(search);
+        }
+    };
+
+    private ListGetterHandler<CpUseLimitdt> useLimitdtListGetterHandler = new ListGetterHandler<CpUseLimitdt>() {
+        @Override
+        List<CpUseLimitdt> getListFromDB(CpUseLimitdt search) {
+            return cpuselimitdtDao.getCpUseLimitdts(search);
+        }
+    };
 
     /**
      * 获取当天有效的优惠券活动
+     *
      * @param search
      * @return
      */
     public List<CpActivity> getCpActivityList(CpActivity search) {
-        String key = String.format("activitys_%s_%s_%s", search.getVendorId(), search.getActNo(),
-                search.getSendType());
-        return activityListGetterHandler.getListAndSetCache(key, search,CpActivity.class);
+        String key = String.format("activitys_%s_%s", search.getVendorId(), search.getSendType());
+        return activityListGetterHandler.getListAndSetCache(key, search, CpActivity.class);
     }
 
+    /**
+     * 获取活动下的有效的优惠券
+     *
+     * @param search
+     * @return
+     */
+    public List<CpActsubGroup> getCpActsubGroupList(CpActsubGroup search) {
+        String key = String.format("actSubGruops_%s_%s", search.getVendorId(), search.getActNo());
+        return actsubGroupListGetterHandler.getListAndSetCache(key, search, CpActsubGroup.class);
+    }
+
+    /**
+     * 获取活动的应用范围限制
+     *
+     * @param search
+     * @return
+     */
+    public List<CpApplyLimitdt> getCpApplyLimitdtList(CpApplyLimitdt search) {
+        String key = String.format("applyLimits_%s_%s", search.getVendorId(), search.getOwnRecordCode());
+        return applyLimitdtListGetterHandler.getListAndSetCache(key, search, CpApplyLimitdt.class);
+    }
+
+    /**
+     * 获取活动使用范围限制
+     * @param search
+     * @return
+     */
+    public List<CpUseLimitdt> getCpUseLimitList(CpUseLimitdt search) {
+        String key = String.format("applyLimits_%s_%s", search.getVendorId(), search.getOwnRecordCode());
+        return useLimitdtListGetterHandler.getListAndSetCache(key, search, CpUseLimitdt.class);
+    }
 }
